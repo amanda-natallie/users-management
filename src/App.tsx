cbc11d5831bc760523bc57b29648497eb5822da0
Amanda Oliveira <amanda.natallie.2@gmail.com>
2025-06-26 15:02:47 -0300

    refactor(config): rename global CSS file

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,10 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import { getApiConfig } from '@/utils/env';
 
 function App() {
   const [count, setCount] = useState(0);
-  const apiConfig = getApiConfig();
-  console.clear();
-  console.log('apiConfig', apiConfig);
+
   return (
     <>
       <div>
